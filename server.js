3053866bee285da04ce8f09b727fa88b1a1cb0ac
Joao Vitor Soares de Lima Vieira <jvsoares@ibm.com>
2020-06-05 14:51:24 -0300

    nodemon, cors, rest calls

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 
 
 const app = express();
+
+var corsOptions = {
+    origin: ["http://localhost:4200", "https://localhost:4000"]
+};
+app.use(cors(corsOptions));
+
+
 app.listen(8000, () => {
     console.log("Server is started and listening.")
 });
