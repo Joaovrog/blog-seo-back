e0d549ee431055946e87de8453fbde2fa7672d49
Joao Vitor Soares de Lima Vieira <jvsoares@ibm.com>
2020-06-05 13:59:54 -0300

    first API call

---
+++ mock-articles.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+        id: 1,
+        title: 'My Gaara article',
+        content: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        description: 'Red hair and demon eyes, folks.',
+        key: 'gaara-first',
+        date: new Date(),
+        imageUrl: 'https://www.comboinfinito.com.br/principal/wp-content/uploads/2018/05/boruto-gaara.jpg'
+    },
+
+    {
+        id: 2,
+        title: 'Shinki article',
+        content: '<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        description: 'He is Gaara`s son.',
+        key: 'shinki-art',
+        date: new Date(),
+        imageUrl: 'https://pm1.narvii.com/6509/f8ede42610c9695bc4500d82d0c6d8a0cbb4c107_00.jpg'
+    }
+];
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 
+
 const app = express();
 app.listen(8000, () => {
     console.log("Server is started and listening.")
 });
 
+app.get("/", function(request, response) {
+    response.send("Hello, Node!");
+});
+
+require("./articles.js")(app);
